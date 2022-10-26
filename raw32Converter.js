9b8b8b8446b8f487486339354985789f6df9d6ee
Boris Vasilyev <1024sparrow@gmail.com>
2022-10-26 05:25:29 +0300

    raw32Converter.js added

---
+++ raw32Converter.js
@@ -0,0 +1,76 @@
+#!/usr/bin/node
+
+var
+	state = 'stateInitial',
+	args = {
+		video: '',
+		output: ''
+	}
+;
+
+for (const oArg of process.argv.slice(2)){
+	if (oArg === '--help'){
+		process.stdout.write(`Программа Raw32Converter - конвертер видеофайлов в raw32-формат
+
+Raw32Converter --video <путь_до_видеофайла> --output <в_какой_файл_записать_raw32>
+`);
+		process.exit(0);
+	}
+}
+
+function ERROR(a_descr){
+	process.stderr.write(a_descr + '\n');
+	process.exit(1);
+}
+
+for (const oArg of process.argv.slice(2)){
+	//console.log(`state("${state}") arg("${oArg}")`);//
+	if (state === 'stateInitial'){
+		if (oArg.slice(0, 2) === '--' && args.hasOwnProperty(oArg.slice(2))){
+			const name = oArg.slice(2)[0].toUpperCase() + oArg.slice(2).slice(1);
+			if (args[`bArg${name}`]){
+				ERROR(`пресечена попытка повторной установки значения аргумента "${oArg}"`);
+			}
+			args[`bArg${name}`] = true;
+			state = `state${name}`;
+		}
+		else if (oArg[0] === '-'){
+			ERROR(`неподдерживаемый ключ: "${oArg}"`);
+		}
+		else{
+			ERROR(`неожиданный аргумент: "${oArg}"`);
+		}
+	}
+	else {
+		for (const i in args){
+			const name = i[0].toUpperCase() + i.slice(1);
+			if (state === `state${name}`){
+				if (oArg[0] === '-'){
+					ERROR(`ключ "${oArg}": ожидается значение аргумента`);
+				}
+				else{
+					args[i] = oArg;
+					state = 'stateInitial';
+				}
+			}
+		}
+	}
+}
+
+for (const i in args){
+	const name = i[0].toUpperCase() + i.slice(1);
+	if (state === `state${name}`){
+		ERROR(`Аргумент "--${i}": не указано значение`);
+	}
+}
+
+//console.log(JSON.stringify(args, undefined, 4));//
+
+if (args.bArgVideo){
+	if (!args.bArgOutput){
+		ERROR('Не указан обязательный аргумент "--output"');
+	}
+}
+else {
+	ERROR('Не указан режим. Необходимо указать параметр "--video"');
+}
